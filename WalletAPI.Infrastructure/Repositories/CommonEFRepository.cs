dbd78c3838e3fdb215d9e20c12ac363db86f1307
JJovanca82 <59015415+JJovanca82@users.noreply.github.com>
2021-02-24 10:33:58 +0100

    Add files via upload

---
+++ WalletAPI.Infrastructure/Repositories/CommonEFRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalletAPI.Core.Entities;
+using WalletAPI.Core.Interfaces;
+
+namespace WalletAPI.Infrastructure.Repositories
+{
+       
+    public class CommonEFRepository<T>:ICommonEFRepository<T> where T:BaseEntity
+    {
+        protected readonly WalletDbContext _dbContext;
+
+        public CommonEFRepository(WalletDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+        
+    }
+}
---
+++ WalletAPI.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WalletAPI.Core.Entities;
+using WalletAPI.Core.Interfaces;
+
+namespace WalletAPI.Infrastructure.Repositories
+{
+    public class UserRepository : CommonEFRepository<User>,IUserRepository
+    {
+        public UserRepository(WalletDbContext dbContext):base(dbContext)
+        {            
+
+        }
+
+        public Task<Balance> GetBalanceForUserAsync(string guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByGuidAsync(string guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByGuidAsync(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
