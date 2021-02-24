84604fc33d1942bee860d37bc2247403fab47fb9
JJovanca82 <59015415+JJovanca82@users.noreply.github.com>
2021-02-24 10:28:48 +0100

    Add files via upload

---
+++ WalletAPI.Infrastructure/WalletDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Reflection;
+using WalletAPI.Core.Entities;
+
+namespace WalletAPI.Infrastructure
+{
+    public class WalletDbContext:DbContext
+    {
+        public WalletDbContext(DbContextOptions<WalletDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Credentials> Credentials { get; set; }
+
+        public DbSet<Balance> Balances { get; set; }
+
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public DbSet<TransactionType> TransactionTypes { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
